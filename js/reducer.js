6abe51928c1d95596d21d57d52452030609da4b7
sergiocardenasg <sergiocardenas@utexas.edu>
2021-09-12 19:21:59 -0500

    reducer

--- js/reducer.js
+++ js/reducer.js
@@ -1,1 +1,13 @@
 // add code snippets from README
+let state = {count: 0}
+
+function reducer(state, action){
+  switch (action.type) {
+    case 'INCREASE_COUNT':
+      return {count: state.count + 1};
+    default:
+      return state;
+  }
+}
+
+reducer({count: 0}, {type: 'INCREASE_COUNT'});
