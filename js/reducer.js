3fc11afd82d1acc2eeb97350fd956b8cd80ae6a7
sergiocardenasg <sergiocardenas@utexas.edu>
2021-09-12 19:42:39 -0500

    render and default state

--- js/reducer.js
+++ js/reducer.js
@@ -1,15 +1,19 @@
 // add code snippets from README
-let state = {count: 0}
-
-function reducer(state, action){
-  switch (action.type) {
-    case 'INCREASE_COUNT':
-      return {count: state.count + 1};
-    default:
-      return state;
+function reducer(state = {count: 0}, action){
+    switch (action.type) {
+      case 'INCREASE_COUNT':
+        return {count: state.count + 1}
+      default:
+        return state;
+    }
   }
-}
 
-function dispatch(action){
+  function render(){
+    let container = document.getElementById('container');
+    container.textContent = state.count;
+  }
+
+  function dispatch(action){
     state = reducer(state, action);
-}
+    render();
+  }
